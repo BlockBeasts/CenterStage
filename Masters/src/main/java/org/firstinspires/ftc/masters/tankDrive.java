8ff3edf0c91ee5dd91083be39a2afdf73db9e384
rgjava <rgjava10@gmail.com>
2025-09-13 09:23:25 -0600

    added drive train code tele-op

---
+++ Masters/src/main/java/org/firstinspires/ftc/masters/tankDrive.java
@@ -0,0 +1,55 @@
+package org.firstinspires.ftc.masters;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@Config // Enables FTC Dashboard
+//@TeleOp(name = "Starter")
+public class tankDrive extends LinearOpMode {
+
+    private final FtcDashboard dashboard = FtcDashboard.getInstance();
+
+    public static double Blank = 0;
+
+    double forward_power = 0;
+    double turn_power = 0;
+
+    public void runOpMode() throws InterruptedException {
+
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        telemetry.update();
+
+        DcMotor frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        DcMotor backLeft = hardwareMap.dcMotor.get("backLeft");
+        DcMotor frontRight = hardwareMap.dcMotor.get("frontRight");
+        DcMotor backRight = hardwareMap.dcMotor.get("backRight");
+
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            forward_power = gamepad1.left_stick_y;
+            turn_power = gamepad1.left_stick_x;
+
+            frontLeft.setPower(forward_power + turn_power);
+            frontRight.setPower(forward_power - turn_power);
+            backLeft.setPower(forward_power + turn_power);
+            backRight.setPower(forward_power - turn_power);
+
+        }
+    }
+}
+
