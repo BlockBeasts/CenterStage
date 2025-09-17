c113fa2f643b820a087b8e7b87b3e25186406638
rgjava <rgjava10@gmail.com>
2025-09-17 16:37:08 -0600

    changed stuff

--- Masters/src/main/java/org/firstinspires/ftc/masters/components/Intake.java
+++ Masters/src/main/java/org/firstinspires/ftc/masters/components/Intake.java
@@ -2,13 +2,66 @@ package org.firstinspires.ftc.masters.components;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
+
+//intake component prototype
+
+/*
+
+
+
+
+
+
+  
+
+
+*/
+
+
+
+
 public class Intake implements Component{
+
+
+
     public void initializeHardware() {
 
     }
 
     public Intake(Init init, Telemetry telemetry){
 
+
+    }
+
+    public void intakeOn() {
+        //moter stuff! :)
+    }
+
+    public void
+    }
+
+    public void intakeReverse() {
+        //moter stuff! :)
+    }
+
+    public void intakelogic() {
+        //logic! :3
+    }
+
+    public void spinDexerlogic() {
+        //logic! :3
+    }
+
+    public void get_color() {
+        //find the ball colors
+    }
+
+    public void shake() {
+        //shaking logic for finding color
+    }
+
+    public void intakeGoToSpeed() {
+        //makes moter spin at given rpm
     }
 
 }
