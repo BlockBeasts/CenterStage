9d8dbb145d84e6e0f7078e963291968862b4ac9c
rgjava <rgjava10@gmail.com>
2025-10-18 09:48:17 -0600

    added shooter moter code

--- Masters/src/main/java/org/firstinspires/ftc/masters/tankDrive.java
+++ Masters/src/main/java/org/firstinspires/ftc/masters/tankDrive.java
@@ -5,8 +5,11 @@ import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+
 
 @Config // Enables FTC Dashboard
 @TeleOp(name = "tank-drive")
@@ -19,6 +22,9 @@ public class tankDrive extends LinearOpMode {
     double forward_power = 0;
     double turn_power = 0;
 
+    double shootpower = 0;
+    static double speedUpCoef = 0.05;
+
     public void runOpMode() throws InterruptedException {
 
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
@@ -30,6 +36,10 @@ public class tankDrive extends LinearOpMode {
         DcMotor frontRight = hardwareMap.dcMotor.get("frontRight");
         DcMotor backRight = hardwareMap.dcMotor.get("backRight");
 
+        DcMotor shoot = hardwareMap.dcMotor.get("shooter");
+        CRServo pusher1 = hardwareMap.crservo.get("pusher1");
+        CRServo pusher2 = hardwareMap.crservo.get("pusher2");
+
         frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
 
@@ -37,10 +47,27 @@ public class tankDrive extends LinearOpMode {
         frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        shoot.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
 
         waitForStart();
 
         while (opModeIsActive()) {
+
+            if (gamepad2.dpad_up) {
+                if (shootpower != 1) {
+                    shootpower += speedUpCoef;
+                }
+
+            } else if (gamepad2.dpad_down) {
+                if (shootpower != 0) {
+                    shootpower -= speedUpCoef;
+                }
+            }
+
+            //todo add push servo stuff, here!
+
+
 
             forward_power = gamepad1.left_stick_y * -1;
             turn_power = (gamepad1.left_trigger + (gamepad1.right_trigger * -1) * -1);
@@ -49,6 +76,8 @@ public class tankDrive extends LinearOpMode {
             frontRight.setPower(forward_power - turn_power);
             backLeft.setPower(forward_power + turn_power);
             backRight.setPower(forward_power - turn_power);
+
+            shoot.setPower(shootpower);
 
         }
     }
