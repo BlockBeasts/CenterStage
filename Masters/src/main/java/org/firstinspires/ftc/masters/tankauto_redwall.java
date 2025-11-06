2a700c6c7ace0cb1e4c11bc7dd31197a15347b03
rgjava <rgjava10@gmail.com>
2025-11-05 17:59:17 -0700

    auto tank drive code

--- Masters/src/main/java/org/firstinspires/ftc/masters/tankauto_redwall.java
+++ Masters/src/main/java/org/firstinspires/ftc/masters/tankauto_redwall.java
@@ -7,26 +7,44 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorImplEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Config // Enables FTC Dashboard
 @Autonomous(name = "auto-redwall")
 
 public class tankauto_redwall extends LinearOpMode {
+
+
+    DcMotor frontLeft;
+    DcMotor backLeft;
+    DcMotor frontRight;
+    DcMotor backRight;
+    DcMotorImplEx shoot;
+    CRServo pusher1;
+    CRServo pusher2;
+    int targetSpeed = 3000;
+
+    double realSpeed = 0;
+
+
     private final FtcDashboard dashboard = FtcDashboard.getInstance();
 
     public void runOpMode() throws InterruptedException {
+
+        double speedTPS= (double) (28 * targetSpeed) / 60;
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
         telemetry.update();
-        DcMotor frontLeft = hardwareMap.dcMotor.get("frontLeft");
-        DcMotor backLeft = hardwareMap.dcMotor.get("backLeft");
-        DcMotor frontRight = hardwareMap.dcMotor.get("frontRight");
-        DcMotor backRight = hardwareMap.dcMotor.get("backRight");
+        frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        backLeft = hardwareMap.dcMotor.get("backLeft");
+        frontRight = hardwareMap.dcMotor.get("frontRight");
+        backRight = hardwareMap.dcMotor.get("backRight");
 
-        DcMotor shoot = hardwareMap.dcMotor.get("shooter");
-        CRServo pusher1 = hardwareMap.crservo.get("pusher1");
-        CRServo pusher2 = hardwareMap.crservo.get("pusher2");
+        shoot = hardwareMap.get(DcMotorImplEx.class, "shooter");
+        pusher1 = hardwareMap.crservo.get("pusher1");
+        pusher2 = hardwareMap.crservo.get("pusher2");
 
         frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -38,10 +56,37 @@ public class tankauto_redwall extends LinearOpMode {
         shoot.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
 
+
         waitForStart();
 
         while (opModeIsActive()) {
 
+            shoot.setVelocity(speedTPS);
+
+
+            shootBall();
+            shootBall();
+            shootBall();
+
+
+
+
+
+
+        }
+    }
+
+    public void shootBall(){
+        while (realSpeed < targetSpeed && opModeIsActive()) {
+            realSpeed = shoot.getVelocity();
+        }
+        pusher1.setPower(1);
+        pusher2.setPower(1);
+        ElapsedTime elapsedTime = new ElapsedTime();
+
+        while(elapsedTime.milliseconds() < 2000 && opModeIsActive()) {
+            pusher1.setPower(0);
+            pusher2.setPower(0);
         }
     }
 
