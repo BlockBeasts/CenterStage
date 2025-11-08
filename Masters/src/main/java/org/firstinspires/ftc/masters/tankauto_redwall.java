5021767c2ef0efde9713a6ea5b3f2544bc46028e
rgjava <rgjava10@gmail.com>
2025-11-08 10:16:58 -0700

    pedropathing stuff

--- Masters/src/main/java/org/firstinspires/ftc/masters/tankauto_redwall.java
+++ Masters/src/main/java/org/firstinspires/ftc/masters/tankauto_redwall.java
@@ -3,6 +3,12 @@ package org.firstinspires.ftc.masters;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.Path;
+import com.pedropathing.paths.PathChain;
+import com.pedropathing.util.Timer;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
@@ -15,6 +21,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 @Autonomous(name = "auto-redwall")
 
 public class tankauto_redwall extends LinearOpMode {
+    private Path scorePreload;
 
 
     DcMotor frontLeft;
@@ -25,16 +32,24 @@ public class tankauto_redwall extends LinearOpMode {
     CRServo pusher1;
     CRServo pusher2;
     int targetSpeed = 3000;
-
     double realSpeed = 0;
 
+    private Follower follower;
+    private PathChain grabPickup1;
 
+    private final Pose startPose = new Pose(28.5, 128, Math.toRadians(0));
+    private final Pose scorePose = new Pose(72, 128, Math.toRadians(180));
     private final FtcDashboard dashboard = FtcDashboard.getInstance();
 
     public void runOpMode() throws InterruptedException {
 
+
         double speedTPS= (double) (28 * targetSpeed) / 60;
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+
+
+
 
         telemetry.update();
         frontLeft = hardwareMap.dcMotor.get("frontLeft");
@@ -55,23 +70,23 @@ public class tankauto_redwall extends LinearOpMode {
         backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         shoot.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-
+        buildPaths();
 
         waitForStart();
 
         while (opModeIsActive()) {
 
-            shoot.setVelocity(speedTPS);
+            follower.followPath(scorePreload);
+
+            if (!follower.isBusy()) {
+                shoot.setVelocity(speedTPS);
+
+                shootBall();
+                shootBall();
+                shootBall();
 
 
-            shootBall();
-            shootBall();
-            shootBall();
-
-
-
-
-
+            }
 
         }
     }
@@ -88,7 +103,19 @@ public class tankauto_redwall extends LinearOpMode {
             pusher1.setPower(0);
             pusher2.setPower(0);
         }
-    }
 
+
+
+    }
+    public void buildPaths() {
+        scorePreload = new Path(new BezierLine(startPose, scorePose));
+        scorePreload.setLinearHeadingInterpolation(startPose.getHeading(), scorePose.getHeading());
+
+
+        grabPickup1 = follower.pathBuilder()
+                .addPath(new BezierLine(startPose, scorePose))
+                .setLinearHeadingInterpolation(startPose.getHeading(), scorePose.getHeading())
+                .build();
+    }
 
 }
