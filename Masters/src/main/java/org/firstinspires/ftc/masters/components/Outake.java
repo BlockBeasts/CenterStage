79676716951b7fac9908181abd833ba80d4fc9bf
rgjava <rgjava10@gmail.com>
2025-12-20 11:54:32 -0700

    fixed logic issue with outake

--- Masters/src/main/java/org/firstinspires/ftc/masters/components/Outake.java
+++ Masters/src/main/java/org/firstinspires/ftc/masters/components/Outake.java
@@ -17,65 +17,74 @@ public class Outake implements Component{
     DcMotor shootb;
 
     String mode = "off";
-    boolean armed = false;
+
 
     public void initializeHardware() {
 
     }
 
     public Outake(Init init, Telemetry telemetry){
-
         this.init = init;
         this.telemetry=telemetry;
         this.shoota=init.getShootAmoter();
         this.shootb=init.getShootAmoter();
     }
 
-
     private void outakeOn() {
-
         shoota.setTargetPosition(usefullMath.angleToTicks(ITDCons.fireAngle, ITDCons.shootPPR));
         shootb.setTargetPosition(usefullMath.angleToTicks(ITDCons.fireAngle, ITDCons.shootPPR));
-
+        shoota.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        shootb.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        shoota.setPower(1.0);
+        shootb.setPower(1.0);
     }
     private void outakeOff() {
-
         shoota.setTargetPosition(usefullMath.angleToTicks(ITDCons.launchAngle, ITDCons.shootPPR));
         shootb.setTargetPosition(usefullMath.angleToTicks(ITDCons.launchAngle, ITDCons.shootPPR));
+        shoota.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        shootb.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        shoota.setPower(1.0);
+        shootb.setPower(1.0);
+    }
 
+    private boolean isInLaunchPos() {
+        return (shoota.getCurrentPosition() >= usefullMath.angleToTicks(ITDCons.fireAngle, ITDCons.shootPPR)
+                && shootb.getCurrentPosition() >= usefullMath.angleToTicks(ITDCons.fireAngle, ITDCons.shootPPR));
+    }
+    private boolean isInResetPos() {
+        return (shoota.getCurrentPosition() >= usefullMath.angleToTicks(ITDCons.launchAngle, ITDCons.shootPPR)
+                && shootb.getCurrentPosition() >= usefullMath.angleToTicks(ITDCons.launchAngle, ITDCons.shootPPR));
     }
 
     public void launch() {
         mode = "launch";
     }
 
-
-
     public void update() {
-        if (Objects.equals(mode, "launch") || Objects.equals(mode, "reset")) {
 
-
-            if (!armed) {
-                outakeOn();
-                armed = true;
-            } else {
-                if (shoota.getCurrentPosition() >= usefullMath.angleToTicks(ITDCons.fireAngle, ITDCons.shootPPR)
-                        && shootb.getCurrentPosition() >= usefullMath.angleToTicks(ITDCons.fireAngle, ITDCons.shootPPR)) {
-                    outakeOff();
-                    mode = "reset";
-
-
-                } else if (Objects.equals(mode, "reset")) {
-                    if (shoota.getCurrentPosition() >= usefullMath.angleToTicks(ITDCons.launchAngle, ITDCons.shootPPR)
-                            && shootb.getCurrentPosition() >= usefullMath.angleToTicks(ITDCons.launchAngle, ITDCons.shootPPR)) {
-                        mode = "off";
-                        armed = false;
-                    }
-                }
+        if (Objects.equals(mode, "launch")) {
+            outakeOn();
+            if (!shoota.isBusy()) {
+                shoota.setPower(0.0);
+            }
+            if (!shootb.isBusy()) {
+                shootb.setPower(0.0);
+            }
+            if (isInLaunchPos()) {
+                outakeOff();
+                mode = "reset";
+            }
+        }
+        if (Objects.equals(mode, "reset")) {
+            if (!shoota.isBusy()) {
+                shoota.setPower(0.0);
+            }
+            if (!shootb.isBusy()) {
+                shootb.setPower(0.0);
+            }
+            if (isInResetPos()) {
+                mode = "off";
             }
         }
     }
-
-
-
 }
