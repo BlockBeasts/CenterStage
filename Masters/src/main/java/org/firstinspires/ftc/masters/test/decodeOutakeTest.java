7a76c88352c3c86fbc0dfccadb7d7b01cb0f79ca
rgjava <rgjava10@gmail.com>
2026-02-11 16:43:36 -0700

    added lift servos

--- Masters/src/main/java/org/firstinspires/ftc/masters/test/decodeOutakeTest.java
+++ Masters/src/main/java/org/firstinspires/ftc/masters/test/decodeOutakeTest.java
@@ -18,16 +18,26 @@ public class decodeOutakeTest extends LinearOpMode {
     private final FtcDashboard dashboard = FtcDashboard.getInstance();
 
     public static float ServoPos = 0;
+
+    public static float liftLeftServoPos = 0;
+    public static float liftRightServoPos = 0;
+    public static float liftMiddleServoPos = 0;
+
+
     public static int MotorVol = 0;
     public static int IntakeSpeed = 0;
 
-    Servo leftServo, rightServo;
+    Servo leftServo, rightServo, liftLeftServo, liftRightServo, liftMiddleServo;
     DcMotorEx leftMotor, rightMotor, intakeMotor;
 
     public void runOpMode() throws InterruptedException {
 
         leftServo = hardwareMap.get(Servo.class, "leftServo");
         rightServo = hardwareMap.get(Servo.class, "rightServo");
+
+        liftLeftServo = hardwareMap.get(Servo.class, "liftLeftServo");
+        liftRightServo = hardwareMap.get(Servo.class, "liftRightServo");
+        liftMiddleServo = hardwareMap.get(Servo.class, "liftMiddleServo");
 
         leftMotor = hardwareMap.get(DcMotorEx.class, "leftMotor");
         rightMotor = hardwareMap.get(DcMotorEx.class, "rightMotor");
@@ -58,6 +68,19 @@ public class decodeOutakeTest extends LinearOpMode {
                 ServoPos = ServoPos - 10;
             }
 
+            if (gamepad1.left_bumper) {
+                liftLeftServoPos = 90f;
+                liftMiddleServoPos = 90f;
+                liftRightServoPos= 90f;
+            }
+            if (gamepad1.right_bumper) {
+                liftLeftServoPos = 0f;
+                liftMiddleServoPos = 0f;
+                liftRightServoPos= 0f;
+            }
+
+
+
             if(gamepad1.aWasPressed()){
                 IntakeSpeed = 1;
             } else if (gamepad1.bWasPressed()) {
@@ -70,9 +93,13 @@ public class decodeOutakeTest extends LinearOpMode {
             rightMotor.setVelocity(MotorVol);
             intakeMotor.setPower(IntakeSpeed);
 
+            liftLeftServo.setPosition(liftLeftServoPos);
+            liftMiddleServo.setPosition(liftMiddleServoPos);
+            liftRightServo.setPosition(liftRightServoPos);
 
-            //leftServo.setPosition(ServoPos);
-            //rightServo.setPosition(ServoPos);
+
+            leftServo.setPosition(ServoPos);
+            rightServo.setPosition(ServoPos);
 
             telemetry.addData("Current Servo Pos: ", ServoPos);
             telemetry.addData("Current Motor Vol: ", MotorVol);
