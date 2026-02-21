9d940e0313d507304f9ae77149fe430f0174089e
rgjava <rgjava10@gmail.com>
2026-02-21 09:54:13 -0700

    fixed color (Maybe)

--- Masters/src/main/java/org/firstinspires/ftc/masters/components/UsefullFunctions.java
+++ Masters/src/main/java/org/firstinspires/ftc/masters/components/UsefullFunctions.java
@@ -8,9 +8,9 @@ public class UsefullFunctions {
         if (isInRange(green, Constant.minnothingColor, Constant.maxnothingColor)) {
             return "nothing";
         } else if (isInRange(green, Constant.minpurpleColor, Constant.maxpurpleColor)) {
-            return "purple";
-        } else if (isInRange(green, Constant.mingreenColor, Constant.maxgreenColor)) {
             return "green";
+        } else if (isInRange(green, Constant.mingreenColor, Constant.maxgreenColor)) {
+            return "purple";
         }
         return "nothing";
     }
